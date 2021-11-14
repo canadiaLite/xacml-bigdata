71f65869cdfb087267daec7533facc61ddace6c7
emWojo <emwojciech@gmail.com>
2021-11-14 13:31:52 -0600

    update policies and users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const converter = require('./xml2js')
 const moment = require('moment');
 const fs = require('fs');
+const { request } = require('http');
 
 
 class PEP {
@@ -20,13 +21,30 @@ class PDP {
     getUserInfo(id) {
         return PIP.getUserInfo(id)
     }
+    getPolicies(){
+        return PAP.getPolicies()
+    }
+}
+
+class PAP {
+    constructor(policies = []) {
+        var patientPolicy = [this.checkRule1, this.checkRule2];
+        var staffPolicy = [this.checkRule3, this.checkRule4];
+        this.policies = [patientPolicy, staffPolicy];
+    }
+    addPolicy(policy) {
+        this.policies.push(policy)
+    }
+    getPolicies() {
+        return this.policies
+    }
     //rule1()
     checkRule1() {
-      return request.userType == "patient" && (request.resourceType == "imaging" || request.resourceType == "notes");
+    return !(request.userType == "patient" && request.actionType == 'edit' && (request.resourceType == "imaging" || request.resourceType == "notes"));
     }
     //rule2()
     checkRule2(id) {
-        return request.patientID == id && request.resourceType == "personalInfo";
+        return request.patientID == id && request.resourceType == "personalInfo" && request.actionType == 'edit';
     }
     //rule3()
     checkRule3() {
@@ -44,22 +62,23 @@ class PDP {
     }
 }
 
-class PAP {
-    constructor(policies = []) {
-    }
-    addPolicy(policy) {
-        this.policies.push(policy)
-    }
-    getPolicies() {
-        return this.policies
-    }
-}
-
 class PIP {
     constructor(userDict = []) {
+        this.userDict = new Object();
+        this.readUsers();
     }
     readUsers() {
-        //reading each user info from xml, loop through folder
+        const userDir = "users/";
+        const files = fs.readdirSync(userDir);
+        for (const file of files) {
+            var xmlt = fs.readFileSync(userDir+file);
+            var parsedt = converter.parseXml(xmlt);
+            let userAtt = new Object();
+            for (let i=0; i< parsedt.User.Attributes.length; i++){
+                userAtt[parsedt.User.Attributes[i].Category[0].split(":")[7]] = (parsedt.User.Attributes[i].Attribute[0].AttributeValue[0]._);
+            }
+            this.userDict[parsedt.User.id[0]] = userAtt;
+        }
     }
     getUserInfo(id) {
         return this.userDict[id]
@@ -72,3 +91,4 @@ class PIP {
 const xml = fs.readFileSync('test.xml');
 const parsed = converter.parseXml(xml);
 console.log(parsed.breakfast_menu.food[0].name == "Belgian Waffles")
+
